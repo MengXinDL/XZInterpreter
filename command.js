9018bea5feed9ea70e7c51e8a47fdd313fdaafb4
MengXinDL <133215511+MengXinDL@users.noreply.github.com>
2024-01-06 17:37:06 +0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,10 @@
+const commands=[
+    'start', 'over', 'log',                  // 基本操作    √
+    'add', 'mul',                            // 运算操作    √
+    'create', 'write',                       // 储存器操作
+    'if', 'ifnot', 'compair',                // 判断操作
+    'loop',                                  // 循环操作
+    'sleep',                                 // 休止操作
+];
+
+module.exports = commands;
