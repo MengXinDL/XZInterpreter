97ff4f8524da6be1225698ff21f53350fe7980e0
MengXinDL <133215511+MengXinDL@users.noreply.github.com>
2024-01-07 16:22:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const { XZ } = require('./XZ.js');
 
 const X=XZ.create();
 X.command=`start
-create a
-create b
-create c
-write 5.5 &a
-add &a -4 &b
-mul &a &b &c
-log &a &b &c
-ifnot &c
+write 7 &n
+write 5 &m
+write 1 &
+write 1 &k
+write &n
+write 1 &ans
+loop
 start
-log 114514
+mul &ans & &ans
+sub & 1
 over
+sub &n &m
+loop
+start
+mul &k & &k
+sub & 1
+over
+div &ans &k &ans
+write &m
+write 1 &k
+loop
+start
+mul &k & &k
+sub & 1
+over
+div &ans &k &ans
+log &ans
 over`;
 console.log(X.command);
 X.run();
