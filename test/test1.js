a2206d0718c638ee1cf7013e609e1d5f8039ef09
York <helloyork@icloud.com>
2024-01-07 17:20:17 -0800

    init

---
+++ test/test1.js
@@ -0,0 +1,14 @@
+const { XZ } = require('../src/index.js');
+
+const X=XZ.create();
+X.conmmand='start';
+X.conmmand='create a';
+X.conmmand='create b';
+X.conmmand='create c';
+X.conmmand='write 5.5 &a';
+X.conmmand='add &a -4 &b';
+X.conmmand='mul &a &b &c'
+X.conmmand='log &a &b &c'
+X.conmmand='over';
+console.log(X.conmmand);
+X.run();
